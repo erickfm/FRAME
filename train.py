73973c8a04d82b139e081ae85e158f0ed3cdb807
Erick <efm.physics@gmail.com>
2025-06-24 18:12:56 -0700

    more epochs

--- train.py
+++ train.py
@@ -24,7 +24,7 @@ from model   import FramePredictor, ModelConfig
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 BATCH_SIZE      = 256
-NUM_EPOCHS      = 100
+NUM_EPOCHS      = 200
 LEARNING_RATE   = 3e-4
 WEIGHT_DECAY    = 1e-2            # AdamW (weights only)
 NUM_WORKERS     = 16
